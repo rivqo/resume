ddebf7235587a52a73650bb87e7da3f3dc31b1e2
Olaoluwa Sipe <olaoluwa@wiseki.com>
2025-04-25 23:12:47 +0100

    user-end updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
+import ClientProviders from "@/components/client-providers"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -13,9 +14,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-        </ThemeProvider>
+        <ClientProviders>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+          </ThemeProvider>
+        </ClientProviders>
       </body>
     </html>
   )
