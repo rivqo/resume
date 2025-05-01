71879700dadf0d0fe4b4cf3864db64356bb6b876
Olaoluwa Sipe <123474859+olaoluwasipe@users.noreply.github.com>
2025-05-01 14:21:32 +0100

    color update

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ClientProviders>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
             {children}
           </ThemeProvider>
         </ClientProviders>
