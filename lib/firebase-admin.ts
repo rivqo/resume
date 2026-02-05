b4eaa5d9ca449ed87edac33cf2ed3f088fa35cff
Olaoluwa Sipe <123474859+olaoluwasipe@users.noreply.github.com>
2026-02-05 23:24:55 +0100

    updates to CI

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -4,10 +4,12 @@ import { getApps, initializeApp, cert, App } from "firebase-admin/app";
 import { getFirestore, FieldValue } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
 // import serviceAccount from "../serviceKey.json";
+
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
 const serviceAccount = {
   project_id: process.env.FIREBASE_PROJECT_ID,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  private_key: privateKey,
 };
 
 let adminApp: App;
